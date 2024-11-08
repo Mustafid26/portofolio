1736e18a0d1c9273e01a3576e176106cbdb735dc
mustafid kaisalana <mustafidku26@gmail.com>
2024-11-08 14:01:02 +0700

    10

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect , useState} from 'react';
 import { Typewriter } from 'react-simple-typewriter';
+import Confetti from 'react-confetti';
 import './Hero.css';
 import AOS from './AOS.jsx';
 
 function Hero() {
-    const [count, setCount] = useState(0);  
+    
+    const [count, setCount] = useState(() => {
+        const simpanCount = localStorage.getItem('count');
+        return simpanCount ? parseInt(simpanCount, 10) : 0;
+    });  
     const images = [
         '/gwe4.svg',
         '/gwe5.svg'
@@ -29,11 +34,28 @@ function Hero() {
     //     document.title = `Mustafid Kaisalana - ${count}`;
     // }, [count]);
     const handleClick = () => {
-        setCount(count + 1);
+        const newCount = count + 1;
+        setCount(newCount);
+        localStorage.setItem('count', newCount);
     };
+    const handleReset = () => {
+        setCount(0);
+        localStorage.setItem('count', 0); // Reset count in localStorage
+    };
+    let emoji = '';
+    if (count > 20 && count <= 30) {
+        emoji = '⸜(｡˃ ᵕ ˂ )⸝♡';
+    } else if (count > 10 && count <= 20) {
+        emoji = '>_<';
+    } else if (count > 30 && count <= 1000){
+        emoji = '(˶ˆᗜˆ˵)'
+    } else if (count > 1000){
+        emoji = '(˶˃ ᵕ ˂˶) .ᐟ.ᐟ'
+    }
 
     return (
         <div>
+            {count > 1000 && <Confetti numberOfPieces={300} recycle={false} />}
             <div className="hero bg-base-200 px-5" data-aos="fade-up">
                 <div className="lines">
                     <div className="line"></div>
@@ -65,7 +87,10 @@ function Hero() {
                         <p className="py-6 text-1xl" data-aos="fade-right">
                             I am an enthusiastic software developer, passionate about learning and exploring the latest technologies to create innovative solutions
                         </p>
-                        <button className="btn btn-primary" onClick={handleClick} data-aos="fade-right">Count Klick {count}</button>
+                        <button className="btn btn-primary shake " onClick={handleClick} data-aos="fade-right">Count Klick {count} {emoji}</button>
+                        <button className="btn btn-secondary ml-3" onClick={handleReset} data-aos="fade-right">
+                            Reset Count
+                        </button>
                     </div>
                 </div>
             </div>
